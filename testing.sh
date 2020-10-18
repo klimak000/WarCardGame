d06d552eefbc01acace66480c98914726929851b
klimak000 <klimak007@gmail.com>
2020-10-18 23:08:16 +0200

    [0.3.1] fixing running

--- testing.sh
+++ testing.sh
@@ -1,10 +1,12 @@
+#!/usr/bin/env bash
+
 # Static code analysis and running tests scripts.
 
 # pip3 install .
 pytest --cov war --cov tests tests/ --cov-report term-missing
 
 echo "PYLINT:"
-pylint war setup.py main.py
+pylint war setup.py war_card_game_test.py
 pylint tests --disable=C0116
 
 echo "MYPY:"
--- war_card_game_test.py
+++ war_card_game_test.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """ Testing module."""
 
 from war.game import Game
